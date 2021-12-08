7f4161f4311db0f94adfc320fd0fd5a8fe95ddb2
frostzt <aidenfrostbite@gmail.com>
2021-12-08 20:53:13 +0530

    1_money_change complete

--- 1-algorithmic-toolbox/week3/1_money_change/change.cpp
+++ 1-algorithmic-toolbox/week3/1_money_change/change.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
 int get_change(int m) {
-  // write your code here
-  return m;
+  int changes = 0;
+
+  if (m >= 10) {
+    changes = changes + (int)(m / 10);
+    m = m - (int)(changes * 10);
+  }
+
+  if (m >= 5) {
+    changes = changes + (int)(m / 5);
+    m = m - ((int)(m / 5) * 5);
+  }
+
+  if (m >= 1) {
+    changes = changes + m;
+  }
+
+  return changes;
 }
 
 int main() {
