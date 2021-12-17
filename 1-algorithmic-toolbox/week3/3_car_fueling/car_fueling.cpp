cc5d6e8f23e892f695f320b0fb10f5afa254b312
frostzt <aidenfrostbite@gmail.com>
2021-12-17 21:37:54 +0530

    working on car_fueling problem

--- 1-algorithmic-toolbox/week3/3_car_fueling/car_fueling.cpp
+++ 1-algorithmic-toolbox/week3/3_car_fueling/car_fueling.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include <vector>
 
-using std::cin;
-using std::cout;
 using std::vector;
-using std::max;
 
-int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+int compute_min_refills(int dest, int tank, vector<int> &stops) {
+  int size = (int)stops.size() - 1;
+  int currTank = tank;
+  int refills = 0;
+  int currPos = 0;
+
+  while (currPos != size) {
+    // Check if the next position is reachable with the current tank capacity
+    if (stops.at(currPos + 1) - stops.at(currPos) <= currTank) {
+      currPos++;
+      currTank = currTank - (stops.at(currPos) - stops.at(currPos - 1));
+    } else {
+      refills++;
+      currTank = tank;
+    }
+  }
+
+  return refills;
 }
 
-
 int main() {
-    int d = 0;
-    cin >> d;
-    int m = 0;
-    cin >> m;
-    int n = 0;
-    cin >> n;
+  int d = 0;
+  std::cin >> d;
+  int m = 0;
+  std::cin >> m;
+  int n = 0;
+  std::cin >> n;
 
-    vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
-        cin >> stops.at(i);
+  int i = 0;
+  vector<int> stops(n + 2);
+  stops[0] = 0;
+  for (i = 1; i <= n; i++) {
+    std::cin >> stops.at(i);
+  }
+  stops[n + 1] = d;
 
-    cout << compute_min_refills(d, m, stops) << "\n";
+  std::cout << compute_min_refills(d, m, stops) << "\n";
 
-    return 0;
+  return 0;
 }
