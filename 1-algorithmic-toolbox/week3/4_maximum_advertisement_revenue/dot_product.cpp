2e4dff92f6a69e42d10b7ab7cfd506a046213065
frostzt <aidenfrostbite@gmail.com>
2021-12-19 21:25:29 +0530

    dot_product completed

--- 1-algorithmic-toolbox/week3/4_maximum_advertisement_revenue/dot_product.cpp
+++ 1-algorithmic-toolbox/week3/4_maximum_advertisement_revenue/dot_product.cpp
@@ -4,12 +4,21 @@
 
 using std::vector;
 
-long long max_dot_product(int n, vector<int> a, vector<int> b) {
+long long max_dot_product(int n, vector<long long> a, vector<long long> b) {
   long long result = 0;
+  int size = (int)a.size();
 
   if (n == 1) {
     result = a.at(0) * b.at(0);
     return result;
+  }
+
+  // Sort the vectors in descending order
+  std::sort(a.begin(), a.end(), std::greater<int>());
+  std::sort(b.begin(), b.end(), std::greater<int>());
+
+  for (int i = 0; i < size; i++) {
+    result = result + (a.at(i) * b.at(i));
   }
 
   return result;
@@ -18,7 +27,7 @@ long long max_dot_product(int n, vector<int> a, vector<int> b) {
 int main() {
   int n;
   std::cin >> n;
-  vector<int> a(n), b(n);
+  vector<long long> a(n), b(n);
   for (int i = 0; i < n; i++) {
     std::cin >> a[i];
   }
